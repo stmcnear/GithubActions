24cfb79c6a5548861ec0500b6b2b1017b463ce70
stmcnear <111715141+stmcnear@users.noreply.github.com>
2024-04-12 23:01:48 -0500

    Update Program.cs

--- Console/Program.cs
+++ Console/Program.cs
@@ -84,7 +84,9 @@ namespace GithubActionsLab
         // Implement this method following a similar pattern as above
         public static double Power(string x, string y)
         {
-            throw new NotImplementedException();
+            double baseValue = double.Parse(x);
+            double exponent = double.Parse(y);
+            return Math.Pow(baseValue, exponent);
         }
     }
 
